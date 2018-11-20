75c312eb867cc35a0472b4b61346db3605fd523b
linlin <linlin@co-mall.com>
2018-11-20 20:38:25 +0800

    t-[lesson-01]-[项目初始化]

---
+++ util/http.js
@@ -0,0 +1,18 @@
+import {config} from '/config.js'
+
+class HTTP {
+  request (params) {
+
+  }
+  // wx.request({
+  //   url: '',
+  //   data: '',
+  //   header: {},
+  //   method: 'GET',
+  //   dataType: 'json',
+  //   responseType: 'text',
+  //   success: function(res) {},
+  //   fail: function(res) {},
+  //   complete: function(res) {},
+  // })
+}
